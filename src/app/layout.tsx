37dc846f4ab230fb10aab6b7aed04fb150ca6ca8
elpwc/wniko <elpwc@hotmail.com>
2025-10-23 00:00:17 +0900

    feat: Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import App from './app';
 import { LocaleProvider } from '@/utils/hooks/useLocale';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -30,6 +31,7 @@ export default async function RootLayout({
 				<LocaleProvider>
 					<App>{children}</App>
 				</LocaleProvider>
+				<GoogleAnalytics gaId="G-M3D0Z0VV1D" />
 			</body>
 		</html>
 	);
