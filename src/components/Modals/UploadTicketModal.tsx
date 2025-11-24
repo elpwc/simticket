50c61a759c25f745aecea5e58eb4cfe34ca79b4d
elpwc/wniko <elpwc@hotmail.com>
2025-11-24 17:26:32 +0900

    feat: Progressed in UploadTicketModal

--- src/components/Modals/UploadTicketModal.tsx
+++ src/components/Modals/UploadTicketModal.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Modal } from '../InfrastructureCompo/Modal';
 import { useLocale } from '@/utils/hooks/useLocale';
 import { encodeTicket, TicketListItemProperty } from '@/utils/utils';
@@ -6,6 +6,8 @@ import { Field, Form, Formik } from 'formik';
 import './index.css';
 import { TicketViewer } from '../InfrastructureCompo/ticketViewer';
 import { getIP } from '@/utils/api';
+import clsx from 'clsx';
+import { DescriptionButton } from '../InfrastructureCompo/DescriptionButton';
 
 interface Props {
 	show: boolean;
@@ -21,8 +23,13 @@ export const UploadTicketModal = ({ show, ticketInfo, onClose }: Props) => {
 		editorName: '',
 	};
 
-	const [buttonAvailable, setbuttonAvailable]: [boolean, any] = useState(true);
+	const [isAgree, setIsAgree]: [boolean, any] = useState(false);
+	const [buttonAvailable, setbuttonAvailable]: [boolean, any] = useState(false);
 	const [initialValues, setinitialValues]: [any, any] = useState(defaultValues);
+
+	useEffect(() => {
+		setbuttonAvailable(isAgree);
+	}, [isAgree]);
 
 	const handleUpload = async (values: typeof defaultValues) => {
 		await fetch('/api/ticket', {
@@ -49,7 +56,7 @@ export const UploadTicketModal = ({ show, ticketInfo, onClose }: Props) => {
 	};
 
 	return (
-		<Modal title={'upload ticket'} isOpen={show} onClose={onClose}>
+		<Modal title={t('UploadTicketModal.title')} isOpen={show} onClose={onClose}>
 			<div className="flex flex-col"></div>
 			<div>
 				<Formik
@@ -72,16 +79,43 @@ export const UploadTicketModal = ({ show, ticketInfo, onClose }: Props) => {
 								ticketData={ticketInfo.ticketData}
 							/>
 
+							<div className="text-[12px] flex flex-col gap-3">
+								<p>{t('UploadTicketModal.tips.tip1')}</p>
+								<span>
+									{t('UploadTicketModal.tips.tip2')}
+									<DescriptionButton title="为什么？">为什么</DescriptionButton>
+								</span>
+								<span>
+									{t('UploadTicketModal.tips.tip3')}
+									<DescriptionButton title="为什么？">为什么</DescriptionButton>
+								</span>
+							</div>
+
 							<label className="login-form-item-container">
-								<Field id="name" name="name" className="login-input" placeholder="name" />
+								<label className="login-form-checkbox-container">
+									<input
+										type="checkbox"
+										name="agree"
+										className="auto-login-checkbox"
+										checked={isAgree}
+										onChange={(e: any) => {
+											setIsAgree(!isAgree);
+										}}
+									/>
+									<span className="auto-login-label">{t('UploadTicketModal.agreeCheckbox')}</span>
+								</label>
 							</label>
 
 							<label className="login-form-item-container">
-								<Field type="editorName" id="editorName" name="editorName" className="login-input" placeholder="editorName" />
+								<Field id="name" name="name" className="login-input" placeholder={t('UploadTicketModal.name')} />
+							</label>
+
+							<label className="login-form-item-container">
+								<Field type="editorName" id="editorName" name="editorName" className="login-input" placeholder={t('UploadTicketModal.editorName')} />
 							</label>
 
 							<div className="login-form-item-container">
-								<button type="submit" className="retro-button login-button" disabled={!buttonAvailable}>
+								<button type="submit" className={clsx('retro-button login-button', buttonAvailable ? '' : 'disabled')}>
 									投稿
 								</button>
 							</div>
