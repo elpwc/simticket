779776f7a90933e2767cc85d399f08b86ed6f977
elpwc/wniko <elpwc@hotmail.com>
2025-11-28 04:46:21 +0900

    fix & locale: Expand width of editConfirmDialog & i18n in editConfirmDialog

--- src/components/InfrastructureCompo/ticketListViewItem.tsx
+++ src/components/InfrastructureCompo/ticketListViewItem.tsx
@@ -132,6 +132,7 @@ export const TicketListViewItem = ({ width, height, className, borderRadius, tic
 			</AnimatePresence>
 			{/*editConfirmDialog */}
 			<Modal
+				style={{ minWidth: '500px' }}
 				isOpen={showEditComfirmDialog}
 				showCancelButton
 				showCancel2Button
